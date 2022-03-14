b24b8c4ef93f4e41dd05441558c2e41a34bb16ee
Toshir-96 <99965804+Toshir-96@users.noreply.github.com>
2022-03-14 14:13:26 +0530

    contract us fixed

--- docs/php/mail.php
+++ docs/php/mail.php
@@ -1,50 +1,18 @@
-<?php 
- $server = "Localhost";
- $username = "id18607675 _contactus";
- $password = "Hecking.me@19&";
- $dbname = "id18607675_contact_us_message_form";
- 
- 
- $conn = mysqli_connect($server, $username, $password, $dbname);
-
- if(!$conn){
-     die("Connection Failed:".mysqli_connect_error());
-    
-  }
-
-
-
-
-
-
-
-
-
-
-?>
-
-
-
-
-
-
-
 <?php
 //get data from form  
-
 $name = $_POST['name'];
-$email= $_POST['email'];
+$email= $_POST['emailadress'];
 $message= $_POST['message'];
 
 $to = "hecking.me.96@gmail.com";
 
 $subject = "Mail From website";
 $txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
-$headers = "From: noreply@hecking.me" . "\r\n" .
-"CC: gideon.p@hecking.me";
+$headers = "From: contactus@hecking.me" . "\r\n" .
+"CC: ";
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
 }
 //redirect
-header("Location:thankyou.html");
+header("Location:./thankyou.html");
 ?>
